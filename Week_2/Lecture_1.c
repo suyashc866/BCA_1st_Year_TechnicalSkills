6415c81756b3a29df7f58ad09c23f463ae710cf3
Suyash Chaudhary <suyashchaudhary@Suyashs-MacBook-Air.local>
2026-01-29 16:31:24 +0530

    week2_lec_1

---
+++ Week_2/Lecture_1.c
@@ -0,0 +1,212 @@
+// 1. Write a program to display your name on screen
+// 2. Write a program that will read two numbers and display their sum
+// 3. Write a program that will read two numbers and display their multiplication
+// 4. Write a program to read radius of a circle and display its area
+// 5. Write a program that will read principle, rate and time and display simple interest
+
+
+
+
+
+
+// 1. Display Your Name
+
+
+// 	Flowchart
+	
+//     ┌───────┐
+//    │ Start │
+//    └───┬───┘
+//        ↓
+// ┌────────────────┐
+// │ Print Name     │
+// └────────────────┘
+//        ↓
+//    ┌──────┐
+//    │ Stop │
+//    └──────┘
+
+// 	Algorithm
+	
+// 		a. Start
+// 		b. Print name
+// 		c. Stop
+	
+	
+    // C Program
+	
+// #include <stdio.h>
+// int main()
+// {
+//     printf("Suyash Chaudhary");
+//     return 0;
+// }
+
+// ========================================================================================
+
+
+
+// 2. Sum of Two Numbers
+
+
+
+// 	Flowchart
+	
+//    ┌───────┐
+//    │ Start │
+//    └───┬───┘
+//        ↓
+// ┌────────────────┐
+// │ Read a, b      │
+// └────────────────┘
+//        ↓
+// ┌────────────────┐
+// │ sum = a + b    │
+// └────────────────┘
+//        ↓
+// ┌────────────────┐
+// │ Print sum      │
+// └────────────────┘
+//        ↓
+//    ┌──────┐
+//    │ Stop │
+//    └──────┘
+	
+	
+	
+	// Algorithm
+	
+	
+	// 	a. Start
+	// 	b. Read two numbers
+	// 	c. Calculate sum
+	// 	d. Print sum
+	// 	e. Stop
+	
+	
+	// C Program
+	
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b, sum;
+//     scanf("%d%d", &a, &b);
+//     sum = a + b;
+//     printf("Sum = %d", sum);
+//     return 0;
+// }
+
+
+// ========================================================================================
+
+// 3. Multiplication of Two Numbers
+
+// 	Flowchart
+	
+// 	   Start
+//      ↓
+//   Read a, b
+//      ↓
+//   mul = a * b
+//      ↓
+//   Print mul
+//      ↓
+//    Stop
+// 	Algorithm
+// 		a. Start
+// 		b. Read two numbers
+// 		c. Multiply numbers
+// 		d. Print result
+// 		e. Stop
+	
+	
+// 	C Program
+	
+// #include <stdio.h>
+// int main()
+// {
+//     int a, b, mul;
+//     scanf("%d%d", &a, &b);
+//     mul = a * b;
+//     printf("Multiplication = %d", mul);
+//     return 0;
+// }
+	
+
+// ========================================================================================
+
+// 4. Area of Circle
+
+// 	Flowchart
+	
+// 	   Start
+//      ↓
+//    Read r
+//      ↓
+//  area = 3.14*r*r
+//      ↓
+//  Print area
+//      ↓
+//    Stop
+// 	Algorithm
+	
+// 		a. Start
+// 		b. Read radius
+// 		c. Calculate area
+// 		d. Print area
+// 		e. Stop
+		
+// 	C Program
+	
+// #include <stdio.h>
+// int main()
+// {
+//     float r, area;
+//     scanf("%f", &r);
+//     area = 3.14 * r * r;
+//     printf("Area = %f", area);
+//     return 0;
+// }
+
+
+// ========================================================================================
+
+
+
+// 5. Simple Interest
+
+// 	Flowchart 
+	
+// 	   Start
+//      ↓
+//  Read p, r, t
+//      ↓
+//  SI = (p*r*t)/100
+//      ↓
+//   Print SI
+//      ↓
+//    Stop
+// 	Algorithm
+	
+// 		a. Start
+// 		b. Read p, r, t
+// 		c. Calculate SI
+// 		d. Print SI
+// 		e. Stop
+		
+// 	C Program
+	
+// #include <stdio.h>
+// int main()
+// {
+//     float p, r, t, si;
+//     scanf("%f%f%f", &p, &r, &t);
+//     si = (p * r * t) / 100;
+//     printf("Simple Interest = %f", si);
+//     return 0;
+// }
+	
+
+
+// ========================================================================================
+	
